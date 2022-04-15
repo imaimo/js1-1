fa51bebeee5863554d951f08e2ab318e1e393054
imaimo <kawamura.gaako@gmail.com>
2022-04-15 21:32:40 +0900

    6回目のコミット

--- main.js
+++ main.js
@@ -1,10 +1,26 @@
-/*for (let i = 0; i < 10; i++) {
-    console.log(i)
-}*/
-
-let s = 2
-while (s < 100000) {
-    console.log(s)
-    s = s * s
-
+function cook(food1, food2) {
+    console.log(food1 + 'を切ります。')
+    console.log(food1 + 'を炒めます。')
+    console.log(`${food2}をすりおろします。`)
+    console.log(`${food1}に${food2}を混ぜます。`)
 }
+cook('豚肉', '生姜')
+
+function foo(a, b, c) {
+    console.log(`fooが引数${a}, ${b}, ${c}で呼び出された`)
+}
+
+foo(1, 'abc', 'true')
+
+function bar() {
+    console.log(`barが呼び出された`)
+}
+bar()
+
+function applyTax(price) {
+    const result = price * 0.1
+    return result
+}
+
+const tax = applyTax(12000)
+console.log(tax)
