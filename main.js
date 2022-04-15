7d420fe27df475d7e5ad58627951ad40695cb958
imaimo <kawamura.gaako@gmail.com>
2022-04-15 20:56:45 +0900

    5回目のコミット

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-if (true) {
-    console.log('必ずこちらが実行される')
-} else {
-    console.log('こちらは実行されない')
-}
+/*for (let i = 0; i < 10; i++) {
+    console.log(i)
+}*/
 
-if (false) {
-    console.log('今度はこちらが実行されない')
-} else {
-    console.log('こちらが実行される')
+let s = 2
+while (s < 100000) {
+    console.log(s)
+    s = s * s
+
 }
